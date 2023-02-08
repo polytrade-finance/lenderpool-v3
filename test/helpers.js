e35e24bab34dde54929d53d334e376e65d403e74
saxon <1@1.com>
2023-02-08 19:37:41 +0330

    ✅ add helper functions

--- test/helpers.js
+++ test/helpers.js
@@ -5,3 +5,19 @@ module.exports.now = async function now() {
     await ethers.provider.getBlock(await ethers.provider.getBlockNumber())
   ).timestamp;
 };
+
+module.exports.fromStable = async function fromStable(amount) {
+  return ethers.utils.formatUnits(amount, "6");
+};
+
+module.exports.toStable = async function toStable(amount) {
+  return ethers.utils.parseUnits(amount, "6");
+};
+
+module.exports.fromBonus = async function toBonus(amount) {
+  return ethers.utils.formatUnits(amount, "18");
+};
+
+module.exports.toBonus = async function toBonus(amount) {
+  return ethers.utils.parseUnits(amount, "18");
+};
