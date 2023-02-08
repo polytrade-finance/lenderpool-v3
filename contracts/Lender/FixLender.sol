7c06ec0922e49b47955ddca656bbf7785c1658b0
saxon <1@1.com>
2023-02-08 12:40:30 +0330

    🩹 fix import

--- contracts/Lender/FixLender.sol
+++ contracts/Lender/FixLender.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "../Token/Interface/IToken.sol";
-import "./Interface/IFixLender.sol";
+import "contracts/Token/Interface/IToken.sol";
+import "contracts/Lender/Interface/IFixLender.sol";
 
 /**
  * @title Fixed Lender Pool contract
