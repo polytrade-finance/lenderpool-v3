e03538bf7c502761c666c3516667629cee1bb43f
saxon <1@1.com>
2023-02-16 17:01:21 +0330

    🔨 update interface

--- contracts/Verification/Interface/IVerification.sol
+++ contracts/Verification/Interface/IVerification.sol
@@ -6,25 +6,6 @@ pragma solidity ^0.8.17;
  * @author Polytrade
  */
 interface IVerification {
-    struct UserStatus {
-        bytes2 provider;
-        bool status;
-    }
-
-    /**
-     * @notice Emits when a agent is set (added, removed)
-     * @param agent is the address of the agent to be added or removed
-     * @param status is true if added or false if removed
-     */
-    event AgentSet(address indexed agent, bool status);
-
-    /**
-     * @notice Emits when a user is validated or removed
-     * @param user is the address of the user to be validated or removed
-     * @param provider is the code of the provider (bytes2)
-     * @param status is true if added or false if removed
-     */
-    event ValidationSet(address indexed user, bytes2 provider, bool status);
 
     /**
      * @notice Returns whether a user's KYC is verified or not
