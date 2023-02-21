1a9f2688bd707dcf2b80601d52d93938bb20d76c
saxon <1@1.com>
2023-02-21 11:22:36 +0330

    🔨 add AAVE constants

--- test/constants/constants.helpers.js
+++ test/constants/constants.helpers.js
@@ -1,6 +1,9 @@
 const { ethers } = require("hardhat");
 
 const ZeroAddress = ethers.constants.AddressZero;
+const LenderPoolAccess = ethers.utils.keccak256(
+  ethers.utils.toUtf8Bytes("LENDER_POOL")
+);
 const SampleAPR = 1000; // 10.00%
 const SampleRate = 100; // 1.00
 const StableDecimal = 6;
@@ -10,10 +13,15 @@ const MinDeposit = 100;
 const PoolMaxLimit = 10000;
 const DAY = 1 * 60 * 60 * 24;
 const YEAR = 365 * 60 * 60 * 24;
+const TestaUSDCAddress = "0x9daBC9860F8792AeE427808BDeF1f77eFeF0f24E";
+const TestUSDCAddress = "0xe9DcE89B076BA6107Bb64EF30678efec11939234";
+const TestAAVEPool = "0x0b913A76beFF3887d35073b8e5530755D60F78C7";
+const AccountToImpersonateUSDC = "0xb00b414f9e45ba73b44ffc3e3ce64a806552cd02";
 module.exports = {
   StableDecimal,
   BonusDecimal,
   ZeroAddress,
+  LenderPoolAccess,
   SampleAPR,
   SampleRate,
   SamplePeriod,
@@ -21,4 +29,8 @@ module.exports = {
   PoolMaxLimit,
   DAY,
   YEAR,
+  TestaUSDCAddress,
+  TestUSDCAddress,
+  TestAAVEPool,
+  AccountToImpersonateUSDC,
 };
