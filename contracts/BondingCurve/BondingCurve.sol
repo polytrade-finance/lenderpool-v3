f077c66a3ca68bc8914524cf38f7ab7a9da11e4e
saxon <1@1.com>
2023-02-28 16:44:30 +0330

    🔨 add bonding curve contract

--- contracts/BondingCurve/BondingCurve.sol
+++ contracts/BondingCurve/BondingCurve.sol
@@ -8,12 +8,11 @@ import "contracts/BondingCurve/Interface/IBondingCurve.sol";
  * @author Polytrade
  */
 contract AprBondingCurve is IBondingCurve {
-
     uint256 private _p1;
     uint256 private _p2;
     uint256 private _p3;
     uint256 private _multiple;
-    
+
     /**
      * @param p1_ is the first parameter in polynomial bonding curve function
      * @param p2_ is the second parameter in polynomial bonding curve function
@@ -30,8 +29,12 @@ contract AprBondingCurve is IBondingCurve {
     /**
      * @dev See {IBondingCurve-getRate}.
      */
-    function getRate(uint256 lockingDuration) external view returns(uint256) {
-        uint256 result = _p1 * (lockingDuration ** 2) - _p2 * lockingDuration + _p3;
-        return result / _multiple; 
+    function getRate(uint256 lockingDuration) external view returns (uint256) {
+        uint256 result = _p1 *
+            (lockingDuration ** 2) -
+            _p2 *
+            lockingDuration +
+            _p3;
+        return result / _multiple;
     }
 }
