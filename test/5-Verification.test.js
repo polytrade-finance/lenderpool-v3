f3db6efb11e9db77c770f7273035f138eca17f47
saxon <1@1.com>
2023-03-09 21:35:53 +0330

    ✅ update verification tests

--- test/5-Verification.test.js
+++ test/5-Verification.test.js
@@ -23,6 +23,8 @@ describe("Verification", function () {
   let lenderContract;
   let verification;
   let strategy;
+  let flexLenderContract;
+  let FlexLenderFactory;
   let polytradeProxy;
   let LenderFactory;
   let stableToken;
@@ -37,6 +39,7 @@ describe("Verification", function () {
     accounts = await ethers.getSigners();
     addresses = accounts.map((account) => account.address);
     LenderFactory = await ethers.getContractFactory("FixLender");
+    FlexLenderFactory = await ethers.getContractFactory("FlexLender");
     await hre.network.provider.request({
       method: "hardhat_impersonateAccount",
       params: [AccountToImpersonateUSDC],
@@ -58,6 +61,14 @@ describe("Verification", function () {
     await verification.deployed();
     const Strategy = await ethers.getContractFactory("Strategy");
     strategy = await Strategy.deploy(AAVEPool, USDCAddress, aUSDCAddress);
+    flexLenderContract = await FlexLenderFactory.deploy(
+      addresses[0],
+      USDCAddress,
+      bonusAddress,
+      MinDeposit,
+      PoolMaxLimit
+    );
+    await flexLenderContract.deployed();
     lenderContract = await LenderFactory.deploy(
       addresses[0],
       USDCAddress,
@@ -74,11 +85,16 @@ describe("Verification", function () {
     await lenderContract.deployed();
     await lenderContract.switchStrategy(strategy.address);
     await strategy.grantRole(LenderPoolAccess, lenderContract.address);
+    await flexLenderContract.switchStrategy(strategy.address);
+    await strategy.grantRole(LenderPoolAccess, flexLenderContract.address);
   });
 
   it("Should fail if verification address is zero", async () => {
     await expect(
       lenderContract.switchVerification(ZeroAddress)
+    ).to.be.revertedWith("Invalid Verification Address");
+    await expect(
+      flexLenderContract.switchVerification(ZeroAddress)
     ).to.be.revertedWith("Invalid Verification Address");
   });
 
@@ -93,23 +109,60 @@ describe("Verification", function () {
         32
       )}`
     );
+    await expect(
+      flexLenderContract
+        .connect(accounts[1])
+        .switchVerification(verification.address)
+    ).to.be.revertedWith(
+      `AccessControl: account ${addresses[1].toLowerCase()} is missing role ${ethers.utils.hexZeroPad(
+        ethers.utils.hexlify(0),
+        32
+      )}`
+    );
   });
 
-  it("Should change verification contract for Fix lender", async () => {
+  it("Should change verification contract for Fix and Flex lender", async () => {
+    await flexLenderContract.switchVerification(verification.address);
     await lenderContract.switchVerification(verification.address);
     expect(await lenderContract.verification()).to.be.equal(
       verification.address
     );
   });
 
+  it("Should fail to change verification status without admin access for Flex lender", async () => {
+    await expect(
+      flexLenderContract.connect(accounts[1]).changeVerificationStatus()
+    ).to.be.revertedWith(
+      `AccessControl: account ${addresses[1].toLowerCase()} is missing role ${ethers.utils.hexZeroPad(
+        ethers.utils.hexlify(0),
+        32
+      )}`
+    );
+  });
+
+  it("Should change verification status for flex lender to true", async () => {
+    await flexLenderContract.changeVerificationStatus();
+  });
+
   it("should fail to deposit stable token without KYC", async function () {
     const amount = await toStable("100");
-    await stableToken.transfer(addresses[1], amount);
+    await stableToken.transfer(addresses[1], 3 * amount);
     await stableToken
       .connect(accounts[1])
-      .approve(lenderContract.address, amount);
+      .approve(lenderContract.address, 2 * amount);
+    await stableToken
+      .connect(accounts[1])
+      .approve(flexLenderContract.address, amount);
     await expect(
       lenderContract.connect(accounts[1]).deposit(amount)
+    ).to.be.revertedWith("You are not verified");
+    await expect(
+      flexLenderContract.connect(accounts[1])["deposit(uint256)"](amount)
+    ).to.be.revertedWith("You are not verified");
+    await expect(
+      flexLenderContract
+        .connect(accounts[1])
+        ["deposit(uint256,uint256)"](amount, 180)
     ).to.be.revertedWith("You are not verified");
   });
 
@@ -120,6 +173,11 @@ describe("Verification", function () {
     await expect(lenderContract.connect(accounts[1]).deposit(amount))
       .to.emit(lenderContract, "Deposited")
       .withArgs(addresses[1], amount);
+    await expect(
+      flexLenderContract.connect(accounts[1])["deposit(uint256)"](amount)
+    )
+      .to.emit(flexLenderContract, "BaseDeposited")
+      .withArgs(addresses[1], amount);
   });
 
   it("Should revoke acc1 KYC and acc1 should not able to deposit", async function () {
@@ -129,5 +187,12 @@ describe("Verification", function () {
     await expect(
       lenderContract.connect(accounts[1]).deposit(amount)
     ).to.be.revertedWith("You are not verified");
+    await expect(
+      flexLenderContract.connect(accounts[1])["deposit(uint256)"](amount)
+    ).to.be.revertedWith("You are not verified");
+  });
+
+  it("Should change verification status for flex lender to false", async () => {
+    await flexLenderContract.changeVerificationStatus();
   });
 });
